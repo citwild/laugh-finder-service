dc268ef4aa565632b6c3d5f843e5600388711547
Miles Dowe <milesdowe@gmail.com>
2017-02-11 20:00:34 -0800

    Fix missing class error

---
+++ src/main/java/edu/uw/citw/persistence/domain/InstanceParticipant.java
@@ -0,0 +1,9 @@
+package edu.uw.citw.persistence.domain;
+
+/**
+ * A participant in laughter, the type of laughter, and its intensity.
+ *
+ * Created by miles on 2/11/17.
+ */
+public class InstanceParticipant {
+}
--- src/main/java/edu/uw/citw/persistence/repository/InstanceParticipantsRepository.java
+++ src/main/java/edu/uw/citw/persistence/repository/InstanceParticipantsRepository.java
@@ -1,6 +1,10 @@
 package edu.uw.citw.persistence.repository;
 
+import edu.uw.citw.persistence.domain.InstanceParticipant;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  * For interacting with the LAUGH_INSTANCE_PARTICIPANTS table.
@@ -9,4 +13,6 @@ import org.springframework.data.repository.CrudRepository;
  */
 public interface InstanceParticipantsRepository extends CrudRepository<InstanceParticipant, Long> {
 
+    @Query(value = "select * from laugh_instance_participants where instance_id = ?1", nativeQuery = true)
+    List<InstanceParticipant> findByInstanceId(Integer id);
 }
