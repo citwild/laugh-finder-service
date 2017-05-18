0deda5fce7256f0bcd4c3fef624553c64d4ad91d
Miles Dowe <milesdowe@gmail.com>
2017-05-18 07:11:15 -0700

    Begin work on Present objects

---
+++ src/main/java/edu/uw/citw/persistence/domain/Present.java
@@ -0,0 +1,10 @@
+package edu.uw.citw.persistence.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "present")
+public class Present {
+
+}
---
+++ src/main/java/edu/uw/citw/service/PresentService.java
@@ -0,0 +1,11 @@
+package edu.uw.citw.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PresentService {
+
+    private static final Logger log = LoggerFactory.getLogger(PresentService.class);
+}
