9f2bf912ab62ea551025060dd42b1d6713608b74
Miles Dowe <milesdowe@gmail.com>
2018-01-01 11:03:16 -0800

    Begin adding model endpoints

---
+++ src/main/java/edu/uw/citw/controller/ModelController.java
@@ -0,0 +1,59 @@
+package edu.uw.citw.controller;
+
+import edu.uw.citw.service.model.ModelService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.io.IOException;
+
+/**
+ * For viewing and modifying instances.
+ *
+ * Created by miles on 3/5/17.
+ */
+@CrossOrigin(origins = {"http://localhost:8080", "https://52.37.207.59"})
+@RestController
+@RequestMapping(value = "/model")
+public class ModelController {
+
+    private static final Logger log = LoggerFactory.getLogger(ModelController.class);
+
+    private ModelService modelService;
+
+    @Autowired
+    public ModelController(ModelService modelService) {
+        this.modelService = modelService;
+    }
+
+    @Nullable
+    @ResponseBody
+    @GetMapping(value = "/get/all")
+    public String getAllModels() {
+        log.debug("Retrieving all new models");
+        return modelService.getAllModels();
+    }
+
+    @Nullable
+    @ResponseBody
+    @PostMapping(value = "/changeto/{id}")
+    public String switchModel(
+            @PathVariable(value = "id")
+            @Nonnull Long id)
+    {
+        log.debug("Changing laughter model to ID {}", id);
+
+        return modelService.switchModel(id);
+    }
+
+    @Nullable
+    @ResponseBody
+    @PostMapping(value = "/retrain")
+    public String retrainNewModel() throws IOException {
+        log.debug("Retraining a new model");
+        return modelService.retrainNewModel();
+    }
+}
---
+++ src/main/java/edu/uw/citw/service/model/impl/ModelServiceImpl.java
@@ -0,0 +1,22 @@
+package edu.uw.citw.service.model.impl;
+
+import edu.uw.citw.service.model.ModelService;
+
+public class ModelServiceImpl implements ModelService {
+
+
+    @Override
+    public String getAllModels() {
+        return null;
+    }
+
+    @Override
+    public String switchModel(Long id) {
+        return null;
+    }
+
+    @Override
+    public String retrainNewModel() {
+        return null;
+    }
+}
